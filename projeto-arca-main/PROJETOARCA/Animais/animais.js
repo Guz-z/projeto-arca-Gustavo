6ab92e607ce31df5ffc44bc884cfca160218e22a
Kauã <kauapivett@gmail.com>
2026-06-03 00:52:05 -0300

    Add files via upload

---
+++ projeto-arca-main/PROJETOARCA/Animais/animais.js
@@ -0,0 +1,13 @@
+function filtrarAnimais(tipo) {
+  let cards = document.querySelectorAll('.card-animal');
+
+  cards.forEach(function(card) {
+    if (tipo === 'todos') {
+      card.style.display = 'block';
+    } else if (card.classList.contains(tipo)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
---
+++ projeto-arca-main/PROJETOARCA/Animais/perfil.js
@@ -0,0 +1,5 @@
+let botaoAdotar = document.querySelector('.btn-adotar');
+
+botaoAdotar.addEventListener('click', function() {
+  alert('Você será direcionado para o formulário de adoção.');
+});
